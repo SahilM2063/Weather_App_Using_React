a724bba1ea218e2090f4dc93f7ee2f1cdeeb6134
Sahil Makadia <110756286+SahilM2063@users.noreply.github.com>
2023-12-17 12:25:46 +0530

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,7 +14,8 @@ import AirQuality from "./AirQuality";
 
 const Main = () => {
   // const api_key = "c708f74eaf9742a5bee68d2b538a3945";  // 50 calls/day
-  const api_key = "7c61f0b0e9e44144950676a512791b98"; // 1500 calls/day
+  // const api_key = "7c61f0b0e9e44144950676a512791b98"; // 50 calls/day
+  const api_key = "c4a749ec80b74efbb3dc133c90749474"; // 1500 calls/day
 
   const [wdata, setWdata] = useState({
     weather: {
